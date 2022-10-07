d202be5d461247be77ef386b7e4f03b193d5f952
TatShar <tvku@yandex.ru>
2022-10-07 22:34:19 +0300

    task1

--- Task1.py
+++ Task1.py
@@ -1,24 +1,10 @@
-import decimal
-from decimal import getcontext, Decimal
 
-# Вычислить число c заданной точностью d
+from math import pi
 
-number_str = str(input ('number = '))
-d_str = str(input('Input d= '))
-len_num = len(number_str)
-len_d=len(d_str)
-i=-1
-if len_num>len_d:
-    razn=len_num-len_d
-    print(float(number_str[:-razn]))
-elif len_num<len_d:
-    razn=len_d-len_num
-    while razn>0:
-        number_str=number_str+'0'
-        razn-=1
-    number_dec=Decimal(number_str)
-    print(number_dec)
-else: 
-    number_dec=Decimal(number_str)
-    print (number_dec)
-    
+p=str(pi)
+d= float (input ('d='))
+d1=repr(d)
+l=len(d1)
+print(p[:l])
+
+
