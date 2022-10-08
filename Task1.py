47a07a9499e984febf8390ce7b53727cc3a905fa
TatShar <tvku@yandex.ru>
2022-10-08 19:53:48 +0300

    Task

--- Task1.py
+++ Task1.py
@@ -1,10 +1,18 @@
 
-from math import pi
+# from math import pi
 
-p=str(pi)
-d= float (input ('d='))
-d1=repr(d)
-l=len(d1)
-print(p[:l])
+# p=str(pi)
+# d= float (input ('d='))
+# d1=repr(d)
+# l=len(d1)
+# print(p[:l])
 
+from decimal import Decimal
+
+def accuracy (num,d):
+    number = Decimal(f'{num}')
+    return number.quantize(Decimal(f'{d}'))
+number=float(input('Input a number '))
+d=float(input ('imput bit depth '))
+print (accuracy(number,d))
 
